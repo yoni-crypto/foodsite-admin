1963d1fbd60567ef603c8c37c63fb29e4538d093
yoni-crypto <yonidisu111@gmail.com>
2025-01-28 23:01:38 +0300

    changed localhost

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import 'react-toastify/dist/ReactToastify.css'
 import {ToastContainer}from "react-toastify"
 
 const App = () => {
-  const url="http://localhost:4000"
+  const url="https://foodsite-backend.vercel.app"
   return (
     <div>
       <ToastContainer/>
